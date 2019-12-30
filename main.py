ec160124bfab5407d8423db2becb6ca8db67ff8d
Prasanna Venkatesh <vipranarayan14@gmail.com>
2019-12-30 14:10:15 +0530

    Add save functionality and btn.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from wand.image import Image
 from tkinter import filedialog
 import tkinter
 import base64
+import os
 
 window = tkinter.Tk()
 
 input_preview_canvas = tkinter.Canvas(window, width=500, height=500)
 output_preview_canvas = tkinter.Canvas(window, width=500, height=500)
+
 
 def generate_preview(img):
     img.transform(resize='500x500>')
@@ -22,44 +24,63 @@ def generate_preview(img):
 
     return img_tk
 
+
 def generate_output_preview(img, threshold=0.5):
     img.threshold(threshold)
     return generate_preview(img)
 
+
 def show_input_preview(img):
-    input_preview_canvas.create_image(0, 0, image=generate_preview(img), anchor=tkinter.NW)
+    input_preview_canvas.create_image(
+        0, 0, image=generate_preview(img), anchor=tkinter.NW)
+
 
 def show_output_preview(img):
     global image_on_canvas
-    image_on_canvas = output_preview_canvas.create_image(0, 0, image=generate_output_preview(img), anchor=tkinter.NW)
+    image_on_canvas = output_preview_canvas.create_image(
+        0, 0, image=generate_output_preview(img), anchor=tkinter.NW)
+
 
 def on_open():
     open_dialog = filedialog.Open(window, filetypes=[('All Files', '*.*')])
-    filepath = open_dialog.show()
+    file_path = open_dialog.show()
 
-    if filepath != '':
+    if file_path != '':
         global image
-        image = Image(filename=filepath)
+        image = Image(filename=file_path)
         img = image.clone()
         show_input_preview(img)
         show_output_preview(img)
 
+
 def on_change(val):
-    threshold = float(val)/100
+    global threshold
+    threshold = float(val) / 100
     img_tk = generate_output_preview(image.clone(), threshold)
     output_preview_canvas.itemconfig(image_on_canvas, image=img_tk)
+
+def on_save():
+    file = filedialog.asksaveasfile(mode='w', defaultextension='.jpg')
+    if file:
+        img = image.clone()
+        img.threshold(threshold)
+        img.save(filename=os.path.abspath(file.name))
 
 open_btn = tkinter.Button(window, text='Open')
 open_btn.configure(command=on_open)
 
-threshold_slider = tkinter.Scale(window, from_=25, to=75, orient=tkinter.HORIZONTAL)
+save_btn = tkinter.Button(window, text='Save')
+save_btn.configure(command=on_save)
+
+threshold_slider = tkinter.Scale(
+    window, from_=25, to=75, orient=tkinter.HORIZONTAL)
 threshold_slider.set(50)
 threshold_slider.configure(command=on_change)
 
 open_btn.pack()
+save_btn.pack()
 threshold_slider.pack()
 input_preview_canvas.pack(side=tkinter.LEFT)
 output_preview_canvas.pack(side=tkinter.LEFT)
-
 
 window.mainloop()
