734736562dab254a9399b05b651c4fcc0943199a
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-01-30 23:07:38 +0530

    refactor code acc. to pylint rules.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,11 +1,17 @@
+'''
+Main module.
+'''
+import sys
+
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from app import MainWindow
 
-import sys
-
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = MainWindow()
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+    APPCTXT = ApplicationContext()       # 1. Instantiate ApplicationContext
+
+    WINDOW = MainWindow()
+    WINDOW.show()
+
+    EXIT_CODE = APPCTXT.app.exec_()      # 2. Invoke APPCTXT.app.exec_()
+
+    sys.exit(EXIT_CODE)
